2323b3948d7278ba8faa654608f6a28fec7d0fd3
Christophe Alexandre <christophe.alex@gmail.com>
2022-04-28 14:58:00 +0000

    completely reworked c++ version

--- src/c++/PolluxApp.cpp
+++ src/c++/PolluxApp.cpp
@@ -8,10 +8,13 @@
 #include <filesystem>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 
 #include <grpcpp/grpcpp.h>
 
 #include "pollux_payload.grpc.pb.h"
+
+#include "PolluxAppException.h"
 
 namespace {
 
@@ -33,6 +36,7 @@ void printHelp() {
     << "--port <p>:         Pollux master port" << std::endl
     << "--id <id>:          Pollux payload id" << std::endl
     << "--partitions <nb>:  Global number of partitions" << std::endl 
+    << "--alone:            Do not attempt to connect to Pollux master port" << std::endl
     << "--help:             Displays this help and exit" << std::endl;
   exit(1);
 }
@@ -41,14 +45,12 @@ class ZebulonPayloadClient {
   public:
     ZebulonPayloadClient(
       std::shared_ptr<grpc::Channel> channel,
-      int id,
-      int port):
+      int id):
         stub_(pollux::ZebulonPayload::NewStub(channel)),
-        id_(id),
-        port_(port)
+        id_(id)
     {}
 
-    void sendPayloadReady(int port) {
+    void sendPayloadReady(uint16_t port) {
       grpc::ClientContext context;
       //pollux::PolluxVersion version;
       //version.set_version(pollux::PolluxVersion_Version::PolluxVersion_Version_CURRENT);
@@ -62,7 +64,7 @@ class ZebulonPayloadClient {
         Logger::error("Error while sending \"sendPayloadReady\": " + status.error_message());
         exit(-54);
       }
-      Logger::info("PayloadReady::Response: " + response.info());
+      Logger::info("Response from Zebulon to PayloadReady:: " + response.info());
     }
 
     void polluxCommunication(int id, const std::string& key, const std::string& value) {
@@ -78,13 +80,12 @@ class ZebulonPayloadClient {
     }
 
     std::string getString() const {
-      return "ZebulonPayloadClient id: " + std::to_string(id_) + " port: " + std::to_string(port_);
+      return "ZebulonPayloadClient id: " + std::to_string(id_);
     }
 
   private:
     std::unique_ptr<pollux::ZebulonPayload::Stub> stub_;
     int                                             id_;
-    int                                             port_;
 };
 
 void sendPolluxCommunication(
@@ -148,37 +149,66 @@ class PolluxPayloadService final : public pollux::PolluxPayload::Service {
     ZebulonPayloadClient* zebulonClient_ {nullptr};
 };
 
-int getAvailablePort() {
+struct ServerAddress {
+  char        ip[16];
+  uint16_t    port;
+  std::string getString() const {
+    std::ostringstream stream;
+    stream << ip << ":" << port;
+    return stream.str();
+  }
+};
+
+ServerAddress getAvailableAddress() {
+  Logger::info("Searching available address and port");
   int sock = socket(AF_INET, SOCK_STREAM, 0);
   if (sock == -1) {
-    return -1;
+    throw PolluxAppException("While getting available address, impossible to build socket."); 
   }
 
-  int port = 0; //get next available port
-  struct sockaddr_in sin;
-
-  sin.sin_port = htons(port);
-  sin.sin_addr.s_addr = 0;
-  sin.sin_addr.s_addr = INADDR_ANY;
-  sin.sin_family = AF_INET;
-
-  if (bind(sock, (struct sockaddr *)&sin, sizeof(struct sockaddr_in)) == -1) {
-    if (errno == EADDRINUSE) {
-      std::cerr << "Port in use" << std::endl;
-      return -1;
+  {
+    struct sockaddr_in sin;
+    sin.sin_port = 0; //get next available port
+    sin.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    //sin.sin_addr.s_addr = htonl(INADDR_ANY);
+    sin.sin_family = AF_INET;
+    
+    if (bind(sock, (struct sockaddr *)&sin, sizeof(struct sockaddr_in)) == -1) {
+      std::string errorMessage;
+      if (errno == EADDRINUSE) {
+        errorMessage = "While getting available address: the address is not available. already used by other process";
+      } else {
+        std::stringstream error;
+        error << "While getting available address, error (";
+        error << errno << "): " << strerror(errno);
+        errorMessage = error.str();
+      }
+      Logger::error(errorMessage);
+      throw PolluxAppException(errorMessage);
     }
-    std::cerr << "Current port: " << ntohs(sin.sin_port) << std::endl;
   }
 
   {
     struct sockaddr_in sin;
     socklen_t len = sizeof(sin);
     if (getsockname(sock, (struct sockaddr *)&sin, &len) != -1) {
-      Logger::info("port number " + std::to_string((sin.sin_port)));
-      return ntohs(sin.sin_port);
+      ServerAddress serverAddress;
+      inet_ntop(AF_INET, &sin.sin_addr, serverAddress.ip, sizeof(serverAddress.ip));
+      std::ostringstream stream;
+      stream << "Local ip address: ";
+      stream << serverAddress.ip;
+      Logger::info(stream.str());
+      Logger::info("port number (raw): " + std::to_string((sin.sin_port)));
+      Logger::info("port number: " + std::to_string((ntohs(sin.sin_port))));
+
+      //The ntohs() function converts the unsigned short integer netshort
+      //from network byte order to host byte order.
+      serverAddress.port = ntohs(sin.sin_port);
+      //serverAddress.port = sin.sin_port;
+      return serverAddress;
     }
   }
-  return -1;
+  throw PolluxAppException("getAvailableAddress: unknown error");
 }
 
 }
@@ -186,18 +216,20 @@ int getAvailablePort() {
 int main(int argc, char **argv) {
   GOOGLE_PROTOBUF_VERIFY_VERSION;
 
-  const char* const short_opts = "pin:h::";
+  const char* const short_opts = "pin:ha::";
   const option long_opts[] = {
     {"port",        required_argument,  0,        'p'},
     {"id",          required_argument,  0,        'i'},
     {"partitions",  required_argument,  0,        'n'},
+    {"alone",       no_argument,        nullptr,  'a'},
     {"help" ,       no_argument,        nullptr,  'h'},
     {nullptr,       no_argument,        nullptr,  0}
   };
 
-  int masterPort = -1;
+  int zebulonPort = -1;
   int id = -1;
   int partitions = -1;
+  bool alone = false;
 
   while (true) {
     const auto opt = getopt_long(argc, argv, short_opts, long_opts, nullptr);
@@ -207,13 +239,16 @@ int main(int argc, char **argv) {
 
     switch (opt) {
       case 'p':
-        masterPort = std::stoi(optarg);
+        zebulonPort = std::stoi(optarg);
         break;
       case 'i':
         id = std::stoi(optarg);
         break;
       case 'n':
         partitions = std::stoi(optarg);
+        break;
+      case 'a':
+        alone = true;
         break;
       case 'h':
       default:
@@ -223,7 +258,7 @@ int main(int argc, char **argv) {
     }
   }
 
-  if (masterPort == -1) {
+  if (zebulonPort == -1) {
     std::cout << "port argument is mandatory" << std::endl;
     printHelp();
   }
@@ -236,11 +271,6 @@ int main(int argc, char **argv) {
     printHelp();
   }
 
-  int localID = id;
-  otherIDs = std::vector<int>(partitions);
-  std::iota(otherIDs.begin(), otherIDs.end(), 0);
-  otherIDs.erase(std::remove(otherIDs.begin(), otherIDs.end(), localID), otherIDs.end());
-
   std::string logFileName("pollux-app-" + std::to_string(id) + ".log");
   std::filesystem::path logFilePath(logFileName);
   logFile.open(logFilePath);
@@ -248,50 +278,80 @@ int main(int argc, char **argv) {
   Logger::info(logFileName);
   Logger::info("########################################################");
 
-  int localPort = getAvailablePort();
-  std::string address("localhost");
-  address += ":" + std::to_string(masterPort);
-  Logger::info("creating local client");
-  ZebulonPayloadClient client(
-    grpc::CreateChannel(address, grpc::InsecureChannelCredentials()),
-    localID,
-    localPort
-  );
+  try {
+    int localID = id;
+    otherIDs = std::vector<int>(partitions);
+    std::iota(otherIDs.begin(), otherIDs.end(), 0);
+    otherIDs.erase(std::remove(otherIDs.begin(), otherIDs.end(), localID), otherIDs.end());
 
-  Logger::info("contacting zebulon on " + address + " and sending ready message");
-  //I'm alive send message
-  client.sendPayloadReady(localPort);
+    //ServerAddress serverAddress = getAvailableAddress();
+    ZebulonPayloadClient* zebulonClient = nullptr;
 
-  //create and run local server
-  Logger::info("starting server on " + std::to_string(localPort));
-  std::string serverAddress("localhost:");
-  serverAddress += std::to_string(localPort);
-  PolluxPayloadService service(&client);
-  grpc::ServerBuilder builder;
-  builder.AddListeningPort(serverAddress, grpc::InsecureServerCredentials());
-  //Explore the following later
-  //builder.AddChannelArgument(GRPC_ARG_KEEPALIVE_TIME_MS, 2000);
-  //builder.AddChannelArgument(GRPC_ARG_KEEPALIVE_TIMEOUT_MS, 1000);
-  //builder.AddChannelArgument(GRPC_ARG_HTTP2_BDP_PROBE, 1);
-  //builder.AddChannelArgument(GRPC_ARG_MAX_CONNECTION_IDLE_MS , 1000);
-  builder.RegisterService(&service);
-  std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
-  { 
-    std::ostringstream message;
-    message << "Server listening on " << serverAddress;
-    Logger::info(message.str());
+    std::string zebulonAddress("localhost");
+    zebulonAddress += ":" + std::to_string(zebulonPort);
+
+    if (not alone) {
+      Logger::info("creating local client");
+      zebulonClient = new ZebulonPayloadClient(
+        grpc::CreateChannel(zebulonAddress, grpc::InsecureChannelCredentials()),
+        localID
+      );
+    }
+
+    std::string localServerAddress("localhost:0");
+    //Logger::info("starting server on " + serverAddress.getString());
+    PolluxPayloadService service(zebulonClient);
+    grpc::ServerBuilder builder;
+    //AddListeningPort last optional arg: If not nullptr, gets populated with the port number bound to the grpc::Server
+    //for the corresponding endpoint after it is successfully bound by BuildAndStart(), 0 otherwise.
+    //AddListeningPort does not modify this pointer.
+    int serverPort = 0;
+    builder.AddListeningPort(localServerAddress, grpc::InsecureServerCredentials(), &serverPort);
+    //Explore the following later
+    //builder.AddChannelArgument(GRPC_ARG_KEEPALIVE_TIME_MS, 2000);
+    //builder.AddChannelArgument(GRPC_ARG_KEEPALIVE_TIMEOUT_MS, 1000);
+    //builder.AddChannelArgument(GRPC_ARG_HTTP2_BDP_PROBE, 1);
+    //builder.AddChannelArgument(GRPC_ARG_MAX_CONNECTION_IDLE_MS , 1000);
+    builder.RegisterService(&service);
+    std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+    Logger::info("starting server on " + std::to_string(serverPort));
+    if (not server.get()) {
+      std::ostringstream message;
+      message << "GRPC Server could not be started on " << std::to_string(serverPort);
+      throw PolluxAppException(message.str());
+    }
+
+    if (not alone) {
+      Logger::info("contacting zebulon on " + zebulonAddress + " and sending ready message");
+      //I'm alive send message
+      zebulonClient->sendPayloadReady(serverPort);
+    }
+
+    //create and run local server
+    { 
+      std::ostringstream message;
+      message << "Server listening on " << std::to_string(serverPort);
+      Logger::info(message.str());
+    }
+    server->Wait(); //blocking
+
+    //we get there if PolluxPayloadService was terminated
+    {
+      std::ostringstream message;
+      message << "Server " << std::to_string(serverPort) << " terminated";
+      Logger::info(message.str());
+    }
+
+    delete zebulonClient;
+
+    // Optional:  Delete all global objects allocated by libprotobuf.
+    google::protobuf::ShutdownProtobufLibrary();
+  } catch (PolluxAppException& e) {
+    Logger::error("PolluxAppException: " + e.getReason());
+  } catch (std::exception& e) {
+    Logger::error(e.what());
   }
-  server->Wait(); //blocking
 
-  //we get there if PolluxPayloadService was terminated
-  {
-    std::ostringstream message;
-    message << "Server " << serverAddress << " terminated";
-    Logger::info(message.str());
-  }
-
-  // Optional:  Delete all global objects allocated by libprotobuf.
-  google::protobuf::ShutdownProtobufLibrary();
   Logger::info("End of " + logFileName);
   return 0;
 }
