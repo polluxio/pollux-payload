b8d3800f297c89282ec7a7da7d4d9a939fb8d824
xtof <christophe.alex@gmail.com>
2023-12-20 13:04:10 +0100

    save helloworld testing script

--- src/launcher/helloworld.py
+++ src/launcher/helloworld.py
@@ -1,6 +1,7 @@
 import qarnot
+import os
 
-conn = qarnot.connection.Connection('qarnot.conf')
+conn = qarnot.connection.Connection(os.path.join(os.path.expanduser('~'), 'qarnot.conf'))
 
 task = conn.create_task('Hello World in Python', 'docker-batch', 4)
 
@@ -8,5 +9,5 @@ task.constants['DOCKER_REPO'] = 'ubuntu'
 task.constants['DOCKER_TAG'] = 'latest'
 task.constants['DOCKER_CMD'] = 'echo Hello World from ${INSTANCE_ID}!'
 
-task.run()
+task.submit()
 print(task.stdout())
